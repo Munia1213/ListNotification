b0ca36f67aa4f9ec32c9d313579902fd409cae8c
Munia1213 <rispa2050@gmail.com>
2018-02-25 23:27:18 +0600

    listview using cardview example

---
+++ app/src/main/java/com/example/datahost/listnotification/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.datahost.listnotification;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    ListView lv_languages;
+
+
+    NotificationListAdapter list_adapter;
+    String[] languages = new String[] { "SQL",
+            "JAVA",
+            "JAVA SCRIPT",
+            "C#",
+            "PYTHON",
+            "C++",
+            "PHP",
+            "IOS",
+            "ANDROID"
+    };
+
+    public static int [] language_images=
+
+            {R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.mipmap.ic_launcher};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+        lv_languages.setAdapter(list_adapter);
+
+
+    }
+
+    private void init() {
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("List Cards");
+        list_adapter = new NotificationListAdapter(this,languages, language_images);
+        lv_languages = (ListView) findViewById(R.id.lv_languages);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+// Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+// Handle action bar item clicks here. The action bar will
+// automatically handle clicks on the Home/Up button, so long
+// as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+//noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
